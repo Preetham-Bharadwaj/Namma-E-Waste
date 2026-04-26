677ec2170a3e94ed7ce9435d0778640a8cba35ee
Preetham-Bharadwaj <preethambharadwajbr@gmail.com>
2026-04-26 18:58:10 +0530

    Add databaseURL to Firebase initialization to fix gRPC error

--- api/upload.js
+++ api/upload.js
@@ -16,7 +16,8 @@ const serviceAccount = {
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`,
   });
 }
 
